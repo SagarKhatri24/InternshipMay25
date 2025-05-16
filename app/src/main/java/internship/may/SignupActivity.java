18963b8a159f92866b357c669f2f0d39dc3273fc
Sagar Khatri <khatrisagar2@gmail.com>
2025-05-16 17:28:03 +0530

    Signup Form Validation

--- app/src/main/java/internship/may/SignupActivity.java
+++ app/src/main/java/internship/may/SignupActivity.java
@@ -1,6 +1,16 @@
 package internship.may;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +18,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class SignupActivity extends AppCompatActivity {
+
+    EditText name,email,contact,password,confirmPassword;
+    RadioGroup gender;
+    Spinner country;
+    CheckBox termsCondition;
+    Button signup;
+
+    //String[] countryArray = { "India", "USA", "UK", "Canada" };
+    ArrayList<String> countryArray;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +42,108 @@ public class SignupActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        name = findViewById(R.id.signup_name);
+        email = findViewById(R.id.signup_email);
+        contact = findViewById(R.id.signup_contact);
+        password = findViewById(R.id.signup_password);
+        confirmPassword = findViewById(R.id.signup_confirm_password);
+
+        gender = findViewById(R.id.signup_gender);
+
+        country = findViewById(R.id.signup_country);
+        termsCondition = findViewById(R.id.signup_terms);
+        signup = findViewById(R.id.signup_button);
+
+        gender.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                RadioButton radioButton = findViewById(i);
+                Toast.makeText(SignupActivity.this, radioButton.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        countryArray = new ArrayList<>();
+        countryArray.add("India");
+        countryArray.add("UK");
+        countryArray.add("USA");
+        countryArray.add("Canada");
+        countryArray.add("Demo");
+        countryArray.add("Austrlia");
+
+        countryArray.add(0,"Select Country");
+
+        countryArray.remove(5);
+        countryArray.set(5,"Australia");
+
+        ArrayAdapter adapter = new ArrayAdapter(SignupActivity.this, android.R.layout.simple_list_item_1,countryArray);
+        adapter.setDropDownViewResource(android.R.layout.simple_list_item_checked);
+        country.setAdapter(adapter);
+
+        country.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if(i == 0){
+
+                }
+                else {
+                    Toast.makeText(SignupActivity.this, countryArray.get(i), Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(name.getText().toString().trim().equals("")){
+                    name.setError("Name Required");
+                }
+                else if(email.getText().toString().trim().equals("")){
+                    email.setError("Email Required");
+                }
+                else if(!email.getText().toString().trim().matches(emailPattern)){
+                    email.setError("Valid Email Id Required");
+                }
+                else if(contact.getText().toString().trim().equals("")){
+                    contact.setError("Contact No. Required");
+                }
+                else if(contact.getText().toString().trim().length()<10){
+                    contact.setError("Valid Contact No. Required");
+                }
+                else if(password.getText().toString().trim().equals("")){
+                    password.setError("Password Required");
+                }
+                else if(password.getText().toString().trim().length()<6){
+                    password.setError("Min. 6 Char Password Required");
+                }
+                else if(confirmPassword.getText().toString().trim().equals("")){
+                    confirmPassword.setError("Confirm Password Required");
+                }
+                else if(confirmPassword.getText().toString().trim().length()<6){
+                    confirmPassword.setError("Min. 6 Char Confirm Password Required");
+                }
+                else if(!confirmPassword.getText().toString().trim().matches(password.getText().toString().trim())){
+                    confirmPassword.setError("Password Does Not Match");
+                }
+                else if(gender.getCheckedRadioButtonId() == -1){
+                    Toast.makeText(SignupActivity.this, "Please Select Gender", Toast.LENGTH_SHORT).show();
+                }
+                else if(country.getSelectedItemPosition()<=0){
+                    Toast.makeText(SignupActivity.this, "Please Select Country", Toast.LENGTH_SHORT).show();
+                }
+                else if(!termsCondition.isChecked()){
+                    Toast.makeText(SignupActivity.this, "Please Accept Terms And Condition", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(SignupActivity.this, "Signup Successfully", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }

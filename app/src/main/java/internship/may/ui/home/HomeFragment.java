6bc6cf7cd18befe9dbe51f1ab1d7846a71c2935f
Sagar Khatri <khatrisagar2@gmail.com>
2025-05-22 17:45:27 +0530

    Profile

--- app/src/main/java/internship/may/ui/home/HomeFragment.java
+++ app/src/main/java/internship/may/ui/home/HomeFragment.java
@@ -16,6 +16,19 @@ public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
 
+    String[] nameArray = {"Kilos","Mobiles","Fashion","Electronics","Home & Furniture","Appliances","Flight Bookings","Beauty, Toys & More","Two Wheelers"};
+    String[] imageArray = {
+            "https://rukminim2.flixcart.com/flap/80/80/image/29327f40e9c4d26b.png?q=100",
+            "https://rukminim2.flixcart.com/flap/80/80/image/22fddf3c7da4c4f4.png?q=100",
+            "https://rukminim2.flixcart.com/fk-p-flap/80/80/image/0d75b34f7d8fbcb3.png?q=100",
+            "https://rukminim2.flixcart.com/flap/80/80/image/69c6589653afdb9a.png?q=100",
+            "https://rukminim2.flixcart.com/flap/80/80/image/69c6589653afdb9a.png?q=100",
+            "https://rukminim2.flixcart.com/fk-p-flap/80/80/image/0139228b2f7eb413.jpg?q=100",
+            "https://rukminim2.flixcart.com/flap/80/80/image/71050627a56b4693.png?q=100",
+            "https://rukminim2.flixcart.com/flap/80/80/image/dff3f7adcf3a90c6.png?q=100",
+            "https://rukminim2.flixcart.com/fk-p-flap/80/80/image/05d708653beff580.png?q=100"
+    };
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 

924a1e83d6deb83810792d4939bcc6b2e30e9e50
Sagar Khatri <khatrisagar2@gmail.com>
2025-05-23 17:33:14 +0530

    Category

--- app/src/main/java/internship/may/ui/home/HomeFragment.java
+++ app/src/main/java/internship/may/ui/home/HomeFragment.java
@@ -9,6 +9,10 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
+
+import java.util.ArrayList;
 
 import internship.may.databinding.FragmentHomeBinding;
 
@@ -22,18 +26,37 @@ public class HomeFragment extends Fragment {
             "https://rukminim2.flixcart.com/flap/80/80/image/22fddf3c7da4c4f4.png?q=100",
             "https://rukminim2.flixcart.com/fk-p-flap/80/80/image/0d75b34f7d8fbcb3.png?q=100",
             "https://rukminim2.flixcart.com/flap/80/80/image/69c6589653afdb9a.png?q=100",
-            "https://rukminim2.flixcart.com/flap/80/80/image/69c6589653afdb9a.png?q=100",
+            "https://rukminim2.flixcart.com/flap/80/80/image/ab7e2b022a4587dd.jpg?q=100",
             "https://rukminim2.flixcart.com/fk-p-flap/80/80/image/0139228b2f7eb413.jpg?q=100",
             "https://rukminim2.flixcart.com/flap/80/80/image/71050627a56b4693.png?q=100",
             "https://rukminim2.flixcart.com/flap/80/80/image/dff3f7adcf3a90c6.png?q=100",
             "https://rukminim2.flixcart.com/fk-p-flap/80/80/image/05d708653beff580.png?q=100"
     };
 
+    ArrayList<CategoryList> arrayList;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        //binding.homeCategoryRecycler.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        //binding.homeCategoryRecycler.setLayoutManager(new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL));
+
+        binding.homeCategoryRecycler.setLayoutManager(new StaggeredGridLayoutManager(1,StaggeredGridLayoutManager.HORIZONTAL));
+
+        arrayList = new ArrayList<>();
+        for(int i=0;i<nameArray.length;i++){
+            CategoryList list = new CategoryList();
+            list.setName(nameArray[i]);
+            list.setImage(imageArray[i]);
+            arrayList.add(list);
+        }
+        CategoryAdapter adapter = new CategoryAdapter(getActivity(),arrayList);
+        binding.homeCategoryRecycler.setAdapter(adapter);
+
         return root;
     }
 

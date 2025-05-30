591be5515a8e1804e0a4e267e2d8cb85baca6d6a
Sagar Khatri <khatrisagar2@gmail.com>
2025-05-30 16:06:39 +0530

    Payment Gateway

--- app/src/main/java/internship/may/ProductDetailActivity.java
+++ app/src/main/java/internship/may/ProductDetailActivity.java
@@ -1,11 +1,14 @@
 package internship.may;
 
+import android.app.Activity;
 import android.content.SharedPreferences;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,8 +17,14 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.bumptech.glide.Glide;
+import com.razorpay.Checkout;
+import com.razorpay.PaymentData;
+import com.razorpay.PaymentResultWithDataListener;
 
-public class ProductDetailActivity extends AppCompatActivity {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class ProductDetailActivity extends AppCompatActivity implements PaymentResultWithDataListener {
 
     TextView name,newPrice,oldPrice,discount,unit,description,buyNow;
     ImageView imageView;
@@ -54,9 +63,49 @@ public class ProductDetailActivity extends AppCompatActivity {
         buyNow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                startPayment();
             }
         });
 
+    }
+
+    private void startPayment() {
+        Activity activity = this;
+        Checkout co = new Checkout();
+        co.setKeyID("rzp_test_xsiOz9lYtWKHgF");
+
+        try {
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("name",getResources().getString(R.string.app_name));
+            jsonObject.put("description","Product Purchase");
+            jsonObject.put("send_sms_hash",true);
+            jsonObject.put("allow_rotation",true);
+            jsonObject.put("image",R.mipmap.ic_launcher);
+            jsonObject.put("currency","INR");
+            jsonObject.put("amount",String.valueOf(Integer.parseInt(sp.getString(ConstantSp.PRODUCT_NEW_PRICE,""))*100));
+
+            JSONObject prefill = new JSONObject();
+            prefill.put("email",sp.getString(ConstantSp.EMAIL,""));
+            prefill.put("contact",sp.getString(ConstantSp.CONTACT,""));
+
+            jsonObject.put("prefill",prefill);
+            co.open(activity,jsonObject);
+
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Override
+    public void onPaymentSuccess(String s, PaymentData paymentData) {
+        Log.d("RESPONSE_PAYMENT",s);
+        Toast.makeText(this, "Order Place Successfully", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onPaymentError(int i, String s, PaymentData paymentData) {
+        Log.d("RESPONSE_PAYMENT_ERROR",s);
+        Toast.makeText(this, "Order Place Unsuccessfully", Toast.LENGTH_SHORT).show();
     }
 }
